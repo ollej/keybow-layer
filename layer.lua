7c1a9b3b1f8794bd704dea3290daeb5f03d0c81b
Olle Johansson <olle@wreede.se>
2020-02-23 10:59:52 +0100

    Simple layer switching for keybow

---
+++ layer.lua
@@ -0,0 +1,72 @@
+require "keybow"
+
+-- Key mappings --
+
+layer = {}
+layer.WALKING = { keybow.SPACE, "`", "", "d", "e", "3", "s", "w", "2", "a", "q", "1" }
+layer.FIGHTING = { "0", "+", "", "9", "6", "3", "8", "5", "2", "7", "4", "1" }
+layer.MENU = { "m", "n", "", "o", "p", "r", "i", "c", "z", "x", "l", keybow.ESC }
+layer.layers = { layer.WALKING, layer.FIGHTING, layer.MENU }
+layer.current = 1
+
+function switch_layer(pressed)
+    if pressed then
+        layer.current = layer.current + 1
+        if layer.current > #layer.layers then
+            layer.current = 1
+        end
+    end
+end
+
+function press_layer_key(key_id, pressed)
+    key_action = layer.layers[layer.current][key_id+1]
+    keybow.set_key(key_action, pressed)
+end
+
+function handle_key_00(pressed)
+    press_layer_key(0, pressed)
+end
+
+function handle_key_01(pressed)
+    press_layer_key(1, pressed)
+end
+
+function handle_key_02(pressed)
+    switch_layer(pressed)
+end
+
+function handle_key_03(pressed)
+    press_layer_key(3, pressed)
+end
+
+function handle_key_04(pressed)
+    press_layer_key(4, pressed)
+end
+
+function handle_key_05(pressed)
+    press_layer_key(5, pressed)
+end
+
+function handle_key_06(pressed)
+    press_layer_key(6, pressed)
+end
+
+function handle_key_07(pressed)
+    press_layer_key(7, pressed)
+end
+
+function handle_key_08(pressed)
+    press_layer_key(8, pressed)
+end
+
+function handle_key_09(pressed)
+    press_layer_key(9, pressed)
+end
+
+function handle_key_10(pressed)
+    press_layer_key(10, pressed)
+end
+
+function handle_key_11(pressed)
+    press_layer_key(11, pressed)
+end
