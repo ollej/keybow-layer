da14706701b1289e78763d63c68bd0df5632c4cb
Olle Johansson <olle@wreede.se>
2020-02-23 11:25:53 +0100

    Configurable cycle key

--- layer.lua
+++ layer.lua
@@ -7,6 +7,7 @@ layer.WALKING = { keybow.SPACE, "`", "", "d", "e", "3", "s", "w", "2", "a", "q",
 layer.FIGHTING = { "0", "+", "", "9", "6", "3", "8", "5", "2", "7", "4", "1" }
 layer.MENU = { "m", "n", "", "o", "p", "r", "i", "c", "z", "x", "l", keybow.ESC }
 layer.layers = { layer.WALKING, layer.FIGHTING, layer.MENU }
+layer.cyclekey = 2
 layer.current = 1
 
 function switch_layer(pressed)
@@ -18,55 +19,60 @@ function switch_layer(pressed)
     end
 end
 
-function press_layer_key(key_id, pressed)
+function tap_layer_key(key_id, pressed)
+    if key_id == layer.cyclekey then
+        switch_layer(pressed)
+        return
+    end
+
     key_action = layer.layers[layer.current][key_id+1]
     keybow.set_key(key_action, pressed)
 end
 
 function handle_key_00(pressed)
-    press_layer_key(0, pressed)
+    tap_layer_key(0, pressed)
 end
 
 function handle_key_01(pressed)
-    press_layer_key(1, pressed)
+    tap_layer_key(1, pressed)
 end
 
 function handle_key_02(pressed)
-    switch_layer(pressed)
+    tap_layer_key(2, pressed)
 end
 
 function handle_key_03(pressed)
-    press_layer_key(3, pressed)
+    tap_layer_key(3, pressed)
 end
 
 function handle_key_04(pressed)
-    press_layer_key(4, pressed)
+    tap_layer_key(4, pressed)
 end
 
 function handle_key_05(pressed)
-    press_layer_key(5, pressed)
+    tap_layer_key(5, pressed)
 end
 
 function handle_key_06(pressed)
-    press_layer_key(6, pressed)
+    tap_layer_key(6, pressed)
 end
 
 function handle_key_07(pressed)
-    press_layer_key(7, pressed)
+    tap_layer_key(7, pressed)
 end
 
 function handle_key_08(pressed)
-    press_layer_key(8, pressed)
+    tap_layer_key(8, pressed)
 end
 
 function handle_key_09(pressed)
-    press_layer_key(9, pressed)
+    tap_layer_key(9, pressed)
 end
 
 function handle_key_10(pressed)
-    press_layer_key(10, pressed)
+    tap_layer_key(10, pressed)
 end
 
 function handle_key_11(pressed)
-    press_layer_key(11, pressed)
+    tap_layer_key(11, pressed)
 end
